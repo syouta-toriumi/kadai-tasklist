5b1d1cde55df5deab411f9d4d845337ea428b12f
syouta.toriumi <s.toriumi0911@gmail.com>
2020-04-25 13:23:21 +0000

    add column

--- db/schema.rb
+++ db/schema.rb
@@ -16,12 +16,6 @@ ActiveRecord::Schema.define(version: 2020_04_25_124017) do
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "status"
   end
 
