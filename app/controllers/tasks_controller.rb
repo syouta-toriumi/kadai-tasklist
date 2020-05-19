ca178b90b77eef6e0fde1c7db58f01837b44cfbf
syouta.toriumi <s.toriumi0911@gmail.com>
2020-05-19 11:04:30 +0000

    tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,6 @@ class TasksController < ApplicationController
   before_action :correct_user, only: [:show, :edit, :update, :destroy]
   
   def index
-    # @tasks = Task.order(id: :desc).page(params[:page]).per(3)
     @tasks =current_user.tasks.order(id: :desc).page(params[:page]).per(3)
   end
 
@@ -15,7 +14,6 @@ class TasksController < ApplicationController
   end
 
   def create
-    #@task = Task.new(task_params)
     @task = current_user.tasks.build(task_params)
     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
